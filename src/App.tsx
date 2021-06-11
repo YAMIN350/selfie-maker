0e3f7d4841f542d8d3abfecac597938c9b84f0de
gherbi <yamin.gherbi@display-interactive.com>
2021-06-11 13:13:28 +0200

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import React, {useEffect, useRef, useState } from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 
 import './App.css';
-import { SelfieMaker } from './SelfieMaker';
+import {SelfieMaker, CAMERAS, Contrains} from './SelfieMaker';
+
+
 
 function App() {
     const CANVAS_HEIGHT = 640;
@@ -14,41 +16,26 @@ function App() {
     const redSquare = useRef<HTMLImageElement>(null)
     const debugPanel = useRef<HTMLDivElement>(null);
     const btn_save = useRef<any>(null)
-    
-    const [chooseCamera, setChooseCamera] = useState("user");
-    const [cameras , setCameras ] = useState<Array<MediaDeviceInfo>>()
-    const contrain = {
+
+    const [cameras, setCameras] = useState("user");
+    const contrains: Contrains = {
         audio: false,
         video: {
             width: 1280,
             height: 720,
-            facingMode: chooseCamera
+            facingMode: cameras === CAMERAS.BACK ? {exact: cameras} : cameras
         }
     }
 
     function getChooseCamera(e: any) {
-        setChooseCamera(e.target.value);
+        setCameras(e.target.value);
     }
-    async function getCameras(selfieMaker: SelfieMaker){
-        const devices = async () => await selfieMaker.getCameras();
-        const deviceList = await devices();
-        if (deviceList) {
-            const cameraList = deviceList.devicesInfo.filter( device => device.kind === 'videoinput')
-            if (cameraList){
-                setCameras(cameraList)
-            }
-            if (debugPanel.current) {
-                debugPanel.current.innerHTML = deviceList.devicesInfo.map(d => d.kind).join();
-                debugPanel.current.innerHTML += deviceList.constraints.facingMode;
-            }
-        }
-    }
+
     useEffect(() => {
         const canvasContext = canvas.current!.getContext('2d');
 
-        const selfieMaker = new SelfieMaker(canvasContext!);
-        getCameras(selfieMaker);
-        
+        const selfieMaker = new SelfieMaker(canvasContext!, contrains);
+
         logoAegeanImg.current!.addEventListener("click", () => {
             selfieMaker.setBufferImageSource(logoAegeanImg.current!)
         });
@@ -59,13 +46,13 @@ function App() {
         redSquare.current!.addEventListener("click", () => {
             selfieMaker.setBufferImageSource(redSquare.current!)
         });
-        
+
         canvas.current!.height = CANVAS_HEIGHT;
         canvas.current!.width = CANVAS_WIDTH;
-        
+
         canvas.current!.addEventListener("mouseup", async (e) => {
-            const { left, top } = canvas.current!.getBoundingClientRect();
-            const stamp = { x: e.clientX - left, y: e.clientY - top};
+            const {left, top} = canvas.current!.getBoundingClientRect();
+            const stamp = {x: e.clientX - left, y: e.clientY - top};
             selfieMaker.addStamp(stamp);
 
 
@@ -77,27 +64,29 @@ function App() {
         img_saving.current!.src = dataURL
         btn_save.current!.href = dataURL;
     }
-    
+
     return (
         <div className="App">
             <div className="all-tampons">
-                <img alt="logo aegean" ref={logoAegeanImg} className="logo" src="aegean-logo.png" />
-                <img alt="logo display" ref={logoDisplayImg} className="logo" src="display-logo.png" />
-                <img alt="logo display" ref={redSquare} className="logo" src="red-square.png" />
+                <img alt="logo aegean" ref={logoAegeanImg} className="logo" src="aegean-logo.png"/>
+                <img alt="logo display" ref={logoDisplayImg} className="logo" src="display-logo.png"/>
+                <img alt="logo display" ref={redSquare} className="logo" src="red-square.png"/>
             </div>
-            <select placeholder="--Choose your camera--" onChange={(e) => {getChooseCamera(e);  }} style={{ width: 150, margin: "auto" }} name="camera" id="camera-select">
-                {cameras?.map(camera => <option value={camera.deviceId}>
-                    {camera.deviceId}-{camera.groupId}-{camera.kind}-{camera.label}
-                </option>)}
+            <select placeholder="--Choose your camera--" onChange={(e) => {
+                getChooseCamera(e);
+            }} style={{width: 150, margin: "auto"}} name="camera" id="camera-select">
+                <option value="">--Choose your camera--</option>
+                <option value="user">Front</option>
+                <option value="environment">Back</option>
             </select>
             <video id="video" style={{display: "none"}} ref={video} playsInline autoPlay/>
             <div className="test">
-                <button  onClick={saveImage} ref={btn_save} className="button-save"/>
+                <button onClick={saveImage} ref={btn_save} className="button-save"/>
                 <canvas ref={canvas}/>
                 <img alt="" ref={img_saving} src="" className={"image-saving"}
                      style={{width: CANVAS_WIDTH, height: CANVAS_HEIGHT}}/>
             </div>
-            <div ref={debugPanel} />
+            <div ref={debugPanel}/>
         </div>
     );
 }
--- src/SelfieMaker.ts
+++ src/SelfieMaker.ts
@@ -4,61 +4,69 @@ type PositionMouseDownType = {
     stampBuffer?: CanvasImageSource;
 }
 
-type DeviceCompleteInformation ={
-    devicesInfo :Array<MediaDeviceInfo>,
-    constraints : MediaTrackSupportedConstraints,
+export enum CAMERAS {
+    FRONT = "user",
+    BACK = "environment"
 }
- 
-export class SelfieMaker{
+
+export type Contrains = {
+    audio: boolean,
+    video: {
+        width: number,
+        height: number,
+        facingMode: string | { exact: string }
+    }
+}
+
+export class SelfieMaker {
     stream: MediaStream | undefined;
     positionMouseDown: Array<PositionMouseDownType> = []
     context: CanvasRenderingContext2D;
-    bufferContext: CanvasRenderingContext2D;;
+    bufferContext: CanvasRenderingContext2D;
+;
     stampBuffer: CanvasImageSource | undefined;
-    
-    constructor(context: CanvasRenderingContext2D){
-        this.initCamera();
+
+    constructor(context: CanvasRenderingContext2D, contrains: Contrains) {
+        this.initCamera(contrains);
         const canvasBuffer = document.createElement('canvas');
         this.bufferContext = canvasBuffer.getContext('2d')!;
         this.context = context;
         this.loop();
     }
-    async getCameras(): Promise< DeviceCompleteInformation | undefined>{
-        try{
-            const devicesInfo = await navigator.mediaDevices.enumerateDevices();
-            const constraints = navigator.mediaDevices.getSupportedConstraints();
-            
-            return {devicesInfo, constraints};
-        } catch (e){
-            
-        }
-    }
-    async initCamera() {
-        const contrain = {
-            audio: false,
-            video: { width: 1280, height: 720 }
-        }
+
+    // async getCameras(): Promise< DeviceCompleteInformation | undefined>{
+    //     try{
+    //         const devicesInfo = await navigator.mediaDevices.enumerateDevices();
+    //         const constraints = navigator.mediaDevices.getSupportedConstraints();
+    //
+    //         return {devicesInfo, constraints};
+    //     } catch (e){
+    //
+    //     }
+    // }
+    async initCamera(contrains: Contrains) {
+
         try {
-            this.stream = await navigator.mediaDevices.getUserMedia(contrain);            
+            this.stream = await navigator.mediaDevices.getUserMedia(contrains);
         } catch (e) {
             alert(e);
         }
     }
 
-    async setBufferImageSource(source: HTMLImageElement) : Promise<void>{
+    async setBufferImageSource(source: HTMLImageElement): Promise<void> {
         this.bufferContext.clearRect(0, 0, source.width, source.height)
         this.bufferContext.drawImage(source, 0, 0)
-        const buffer = this.bufferContext.getImageData(0,0, source.width, source.height);
+        const buffer = this.bufferContext.getImageData(0, 0, source.width, source.height);
         this.stampBuffer = await createImageBitmap(buffer);
     }
-    
-    addStamp(stamp : PositionMouseDownType){
+
+    addStamp(stamp: PositionMouseDownType) {
         const stampBuffer = this.stampBuffer;
-        if (stampBuffer){
-            this.positionMouseDown.push({...stamp,  stampBuffer})
+        if (stampBuffer) {
+            this.positionMouseDown.push({...stamp, stampBuffer})
         }
     }
-    
+
     loop() {
         for (let i = 0; i < this.positionMouseDown.length; i++) {
             this.context!.drawImage(this.positionMouseDown[i].stampBuffer!, this.positionMouseDown[i].x, this.positionMouseDown[i].y)
